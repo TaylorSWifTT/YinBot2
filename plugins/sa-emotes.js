26d4a49c89f6a8c95099fcc40bd2850148668381
odorkle <totallyelitehackerman@gmail.com>
2018-07-08 20:12:00 -0400

    added SAEmotes, fixed error reporting in twitter plugins

---
+++ plugins/sa-emotes.js
@@ -0,0 +1,67 @@
+const Discord = require('discord.js');
+const Reporter = require('../lib/reporter.js');
+const cheerio = require('cheerio');
+const http = require('http');
+const request = require('request-promise-native');
+const requestHeaders = require('./shared/request-headers');
+
+const fs = require('fs');
+const path = require('path');
+
+const reporter = new Reporter();
+
+class SaEmotes {
+  constructor(client) {
+    reporter.register({
+      userId: '268183210297393152',
+      client
+    });
+
+    client.on('message', async message => {
+      try {
+        if (/\:(\w+)\:/g.test(message.content)) {
+          const fileExtensions = ['\\.png', '\\.gif']; // escape the '.' because these will be used to build a regex
+          const saEmotesPath = path.join(__rootdir, 'saemotes');
+          const cEmotesPath = path.join(__rootdir, 'cemotes');
+
+          const emoteTags = message.content
+            .match(/:(\w+):/g)
+            .map(tag => '%3A' + tag.slice(1, -1) + '%3A'); // convert colons to URI Encoded colons (wtf?)
+
+          let emoteFilePaths = fs
+            .readdirSync(saEmotesPath)
+            .map(filename => path.join(saEmotesPath, filename))
+            .concat(
+              fs
+                .readdirSync(cEmotesPath)
+                .map(filename => path.join(cEmotesPath, filename))
+            );
+
+          let emote;
+
+          for (let tag of emoteTags) {
+            // check SaEmotes
+            const candidateFilenames = fileExtensions.map(
+              ext => new RegExp(tag + ext)
+            );
+
+            const filepath = emoteFilePaths.find(filename =>
+              candidateFilenames.find(candidateName =>
+                filename.match(candidateName)
+              )
+            );
+
+            if (filepath) {
+              await message.channel.send(new Discord.Attachment(filepath));
+            }
+          }
+        }
+      } catch (e) {
+        e.data = { messageContent: message.content };
+        reporter.error(e);
+      }
+    });
+  }
+}
+
+module.exports = SaEmotes;
